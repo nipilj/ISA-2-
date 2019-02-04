63523dd0e130b0677539768b03888c4622c77794
Nikita Piljavets <nipilj@ttu.ee>
2019-02-04 15:45:01 +0200

    2

--- HW01_15video/Program.cs
+++ HW01_15video/Program.cs
@@ -12,10 +12,15 @@ namespace HW01_15video
         {
             Car myCar = new Car();
 
+
+            /*
             myCar.Make = "Oldmobile";
             myCar.Model = "Cutlas Supreme";
             myCar.Year = 1986;
             myCar.Color = "Silver";
+            */
+
+            //Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
 
             Car myOtherCar;
             myOtherCar = myCar;
@@ -25,6 +30,10 @@ namespace HW01_15video
                 myOtherCar.Model,
                 myOtherCar.Year,
                 myOtherCar.Color);
+
+            myOtherCar.Model = "98";
+
+            //myOtherCar = null;
 
             Console.ReadLine();
 
@@ -37,6 +46,27 @@ namespace HW01_15video
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+        /*
+        public Car()
+        {
+            Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        */
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("Called static MyMethod");
+            Console.WriteLine(Make);
+        }
     }
 }
 
